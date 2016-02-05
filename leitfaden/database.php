4b3adff6ceb0ab40477819b0833ed6a908ac5ab4
Paul Bergmeir <p.bergmeir@gmail.com>
2016-02-05 11:25:33 +0100

    Leitfaden / Testing: Get Database Content

--- leitfaden/database.php
+++ leitfaden/database.php
@@ -2,19 +2,39 @@
     include 'content.php';
 ?>
 
+<!-- GET DATABASE CONTENT -->
+
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$database = "Schule";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+else {
+    echo "Connected successfully <br>";
+}
+
+// Select data
+$sql = "SELECT `Username`, `Password`, `UserID` FROM `post-content` WHERE 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Username: " . $row["Username"]. " - Password: " . $row["Password"]. " - UserID: " . $row["UserID"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
 ?>
 
 
@@ -24,7 +44,7 @@ $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 $path_parts = pathinfo($actual_link);
 
-echo $path_parts['dirname'];
+// echo $path_parts['dirname'];
 
 ?>
 
