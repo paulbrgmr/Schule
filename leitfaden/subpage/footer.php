520e1964abd43fdd47576905910d1685f37ec10a
Paul Bergmeir <p.bergmeir@gmail.com>
2016-02-19 08:49:43 +0100

    Leitfaden: Subpage Footer Lightbox hinzugefügt

--- leitfaden/subpage/footer.php
+++ leitfaden/subpage/footer.php
@@ -2,6 +2,7 @@
         <script src="../_resources/js/jquery.min.js"></script>
         <script src="../_resources/js/bootstrap.min.js"></script>
         <script src="../_resources/js/bigfoot.min.js"></script>
+        <script src="../_resources/js/lightbox.min.js"></script>
         <script src="../_resources/js/main.min.js"></script>
     </body>
 </html>
