8e6c13b921d289a64fb2b19392f1ca7c5268b09a
Paul Bergmeir <p.bergmeir@gmail.com>
2016-03-06 10:26:52 +0100

    WebApp: init

---
+++ webapp/footer.php
@@ -0,0 +1,4 @@
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    </body>
+</html>
