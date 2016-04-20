54d97545378ebd4cbd2d52067955235f45ae9232
Paul Bergmeir <p.bergmeir@gmail.com>
2016-04-20 21:48:16 +0200

    Allgemeine Verbesserungen

--- webapp/head.php
+++ webapp/head.php
@@ -11,24 +11,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 <!-- CSS Links -->
-<!-- Online -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
-
-<!-- Offline -->
-<!-- TODO Delete before final upload -->
-<!-- <link rel="stylesheet" type="text/css" href="_resources/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="_resources/css/jquery.mobile.min.css"> -->
+<!-- Local CSS -->
 <link rel="stylesheet" type="text/css" href="_resources/css/style.min.css">
 
 <!-- JS Links -->
-<!-- Online -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
-<!-- Offline -->
-<!-- TODO Delete before final upload -->
-<!-- <script type="text/javascript" src="_resources/js/jquery.min.js"></script> -->
-<!-- <script type="text/javascript" src="_resources/js/jquery.mobile.min.js"></script> -->
 
 <!-- Font Link -->
 <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
